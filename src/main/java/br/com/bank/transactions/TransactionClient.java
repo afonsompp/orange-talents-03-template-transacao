b221919ef531bbc884305ce8624911fb6351bdd8
Afonso Mateus <afonsoprad10@gmail.com>
2021-04-28 16:28:35 -0400

    endpoint to stop transactions

--- src/main/java/br/com/bank/transactions/TransactionClient.java
+++ src/main/java/br/com/bank/transactions/TransactionClient.java
@@ -1,6 +1,7 @@
 package br.com.bank.transactions;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,4 +11,7 @@ public interface TransactionClient {
 	@RequestMapping(method = RequestMethod.POST, value = "/api/cartoes",
 			consumes = "application/json")
 	void startTransactions(@RequestBody TransactionClientRequest dto);
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/api/cartoes/{id}")
+	void stopTransaction(@PathVariable("id") String id);
 }
--- src/main/java/br/com/bank/transactions/TransactionController.java
+++ src/main/java/br/com/bank/transactions/TransactionController.java
@@ -6,6 +6,8 @@ import feign.FeignException.FeignClientException;
 import feign.FeignException.FeignServerException;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -30,4 +32,13 @@ public class TransactionController {
 		return ResponseEntity.ok().build();
 	}
 
+	@GetMapping("/stop/{id}")
+	public ResponseEntity<?> stopTransaction(@PathVariable("id") String id) {
+		try {
+			client.stopTransaction(id);
+		} catch (FeignClientException | FeignServerException e) {
+			return ResponseEntity.badRequest().build();
+		}
+		return ResponseEntity.ok().build();
+	}
 }
